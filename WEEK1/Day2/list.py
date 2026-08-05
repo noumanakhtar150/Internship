f5f479e414cf8b07f8217a0867a114acef1eb47e
noumanakhtar150 <noumanakhtar150@gmail.com>
2026-08-05 18:22:12 +0500

    first commit

---
+++ WEEK1/Day2/list.py
@@ -0,0 +1,6 @@
+number=[1,2,3,4,5,6,7]
+for num in number:
+    if num%2==0:
+        print(f"The {num} is Even ")
+    else:
+        print(f"{num} is odd")
---
+++ WEEK1/Day3/day 3.py
@@ -0,0 +1,28 @@
+"""Name = 'Nouman'
+Work = 'Software Engineer Intern'
+Place_of_Work = 'Expert System Solutions'
+Date = 3
+Month =  'August'
+
+print(f"My name is {Name} I am working as {Work} at {Place_of_Work} from {Date}rd of {Month}")"""
+#################
+
+"""unArranged_Num = [5,6,89,1,2,4,56]
+
+unArranged_Num.sort()
+print(unArranged_Num)"""
+#################
+
+#################
+"""Colors = {'Red', 'Green', 'Blue'}
+Colors.discard('Green')
+Colors.add('Magenta')
+print(Colors)"""
+#################
+
+
+
+
+
+
+
