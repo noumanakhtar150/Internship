87865f6484783c51ad993e88035a6e1927588bd4
noumanakhtar150 <noumanakhtar150@gmail.com>
2026-08-06 13:05:08 +0500

    day 4 first commit

--- WEEK1/Day4/day4.py
+++ WEEK1/Day4/day4.py
@@ -1,8 +1,11 @@
-users ={
+"""users ={
     'Name' : 'Nouman',
     'Company ': 'Expert System Solution',
     'Post': 'Software Engineer Intern ',
     }
 
 for item in users.values():
-    print (item)
+    print (item)"""
+
+for _ in range(1):
+    print(list(range(10)))
