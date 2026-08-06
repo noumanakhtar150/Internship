e9377e1acacf1c190a7941c6a007ce751643c532
noumanakhtar150 <noumanakhtar150@gmail.com>
2026-08-06 12:57:15 +0500

    Day  4 first commit

--- WEEK1/Day3/day 3.py
+++ WEEK1/Day3/day 3.py
@@ -1,28 +1,6 @@
-"""Name = 'Nouman'
-Work = 'Software Engineer Intern'
-Place_of_Work = 'Expert System Solutions'
-Date = 3
-Month =  'August'
-
-print(f"My name is {Name} I am working as {Work} at {Place_of_Work} from {Date}rd of {Month}")"""
-#################
-
-"""unArranged_Num = [5,6,89,1,2,4,56]
-
-unArranged_Num.sort()
-print(unArranged_Num)"""
-#################
-
-#################
-"""Colors = {'Red', 'Green', 'Blue'}
-Colors.discard('Green')
-Colors.add('Magenta')
-print(Colors)"""
-#################
-
-
-
-
-
-
-
+number = [1,2,3,4,5,6,7]
+for num in number:
+    if num%2==0:
+        print(f"The {num} is Even ")
+    else:
+        print(f"{num} is odd")
---
+++ WEEK1/Day4/day4.py
@@ -0,0 +1,8 @@
+users ={
+    'Name' : 'Nouman',
+    'Company ': 'Expert System Solution',
+    'Post': 'Software Engineer Intern ',
+    }
+
+for item in users.values():
+    print (item)
