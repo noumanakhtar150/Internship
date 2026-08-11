c14355a42daf38fcb47e540b1d757ecce649a3de
noumanakhtar150 <noumanakhtar150@gmail.com>
2026-08-11 18:34:39 +0500

    commit

--- WEEK1/Day2/list.py
+++ WEEK1/Day2/list.py
@@ -1,24 +1,24 @@
-"""Name = 'Nouman'
-Work = 'Software Engineer Intern'
-Place_of_Work = 'Expert System Solutions'
-Date = 3
-Month =  'August'
+# """Name = 'Nouman'
+# Work = 'Software Engineer Intern'
+# Place_of_Work = 'Expert System Solutions'
+# Date = 3
+# Month =  'August'
 
-print(f"My name is {Name} I am working as {Work} at {Place_of_Work} from {Date}rd of {Month}")"""
-#################
+# print(f"My name is {Name} I am working as {Work} at {Place_of_Work} from {Date}rd of {Month}")"""
+# #################
 
-"""unArranged_Num = [5,6,89,1,2,4,56]
+# """unArranged_Num = [5,6,89,1,2,4,56]
 
-unArranged_Num.sort()
-print(unArranged_Num)"""
-#################
+# unArranged_Num.sort()
+# print(unArranged_Num)"""
+# #################
 
-#################
-"""Colors = {'Red', 'Green', 'Blue'}
-Colors.discard('Green')
-Colors.add('Magenta')
-print(Colors)"""
-#################
+# #################
+# Colors = {'Red', 'Green', 'Blue'}
+# Colors.discard('Green')
+# Colors.add('Magenta')
+# print(Colors)
+# #################
 
 
 
---
+++ WEEK2/DAY2/day2.py
@@ -0,0 +1,17 @@
+# num = [1,4,6,8,10]
+# num.extend([5])
+# print(num)
+
+products = ["Apple", "Samsung", "OnePlus"]
+products.append("Motorola")
+#print(products.index("Motorola"))
+
+products.pop(1)
+print(products)
+
+vip = [1,3,4,6,7,8]
+
+reversed=vip[::-1]
+
+print(reversed)
+
