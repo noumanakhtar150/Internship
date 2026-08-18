1f9f69a2e2eae6a16d3cb231d8bacf14594a2450
noumanakhtar150 <noumanakhtar150@gmail.com>
2026-08-18 12:41:05 +0500

    commit

--- WEEK3/DAY1.py
+++ WEEK3/DAY1.py
@@ -1,22 +1,5 @@
-# class Vehicle:
-#     def __init__(self, brand, model, year):
-#         self.brand = brand
-#         self.model = model
-#         self.year = year
-
-# class Ship(Vehicle):
-#     def __init__(self, brand, model, year):
-#         self.brand = brand
-#         self.model = model
-#         self.year = year
-
-# veh1 = Vehicle("BMW", "M8", 2022)
-# ship1 = Ship("Yacht", "Yacht", 2026)
-
-# for x in (veh1, ship1):
-#      print(x.brand, x.model, x.year)
-
 class BankAccount:
+
     def __init__(self, account_title, account_num, balance):
         self.account_title = account_title
         self.__account_num =  account_num
---
+++ WEEK3/DAY2.py
@@ -0,0 +1,12 @@
+# number = [1,2,3,4]
+# my_number =list(map(lambda a : a*2,number))
+
+# for num in my_number:
+#     print(my_number)    
+
+num = []
+
+for i in range(20):
+    num.append(i*2)
+
+print(num)
